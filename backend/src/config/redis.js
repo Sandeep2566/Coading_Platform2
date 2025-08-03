24d94d4ad340f4823b66527dc306987aef167505
SANDEEP25 <sandeep.yd951@gmail.com>
2025-08-04 01:08:16 +0530

    first

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -4,8 +4,8 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-19934.c212.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 19934
+        host: 'redis-17730.c281.us-east-1-2.ec2.redns.redis-cloud.com',
+        port: 17730
     }
 });
 
